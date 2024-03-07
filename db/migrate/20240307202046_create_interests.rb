3deba615394e4f1ec07cf61c6fd68e57d7ff36bd
Samuel Anderson <samuelSD.999@gmail.com>
2024-03-07 20:22:39 +0000

    Generated Interests model

---
+++ db/migrate/20240307202046_create_interests.rb
@@ -0,0 +1,9 @@
+class CreateInterests < ActiveRecord::Migration[7.1]
+  def change
+    create_table :interests do |t|
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_07_201137) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_07_202046) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "interests", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
