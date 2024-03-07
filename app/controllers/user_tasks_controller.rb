ce2a458788570553b3987e0feadd679b40a96b95
Josue Heredia <josue-heredia@outlook.com>
2024-03-07 21:21:23 +0000

    user tasks controller

---
+++ app/controllers/user_tasks_controller.rb
@@ -0,0 +1,68 @@
+class UserTasksController < ApplicationController
+  before_action :set_user_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @user_tasks = UserTask.all
+  end
+
+  def show
+  end
+
+  def new
+    @user_task = UserTask.new
+  end
+
+  def create
+    @user_task = UserTask.new(user_task_params)
+
+    if @user_task.save
+      redirect_to user_tasks_path, notice: 'User task was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user_task.update(user_task_params)
+      redirect_to user_tasks_path, notice: 'User task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user_task.destroy
+    redirect_to user_tasks_path, notice: 'User task was successfully destroyed.'
+  end
+
+  def add_tasks_to_user
+    @user = current_user
+    @tasks = randomly_selected_tasks
+
+    @tasks.each do |task|
+      @user_task = UserTask.new(user: @user, task: task, complete: false)
+      @user_task.save
+    end
+
+    redirect_to user_tasks_path, notice: 'Random tasks were successfully assigned to the user.'
+  end
+
+  private
+
+  def set_user_task
+    @user_task = UserTask.find(params[:id])
+  end
+
+  def user_task_params
+    params.require(:user_task).permit(:user_id, :task_id, :complete)
+  end
+
+  def randomly_selected_tasks
+    interests = current_user.interests.pluck(:id)
+    tasks = Task.where(interest_id: interests).to_a
+    tasks.shuffle.take(5)
+  end
+end
