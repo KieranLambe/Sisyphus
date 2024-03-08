d148dec1b94c81c07fded92add36ead6c8663012
Samuel Anderson <samuelSD.999@gmail.com>
2024-03-08 02:55:41 +0000

    Added basic routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   has_many :user_interests, dependent: :destroy
   has_many :interests, through: :user_interests
-  has_many :user_tasks
+  has_many :user_tasks, dependent: :destroy
   has_many :tasks, through: :user_tasks
 end
