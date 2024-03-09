4c4356a21f67b3af52b16aca1805b479cf332db2
Samuel Anderson <samuelSD.999@gmail.com>
2024-03-09 14:50:25 +0000

    Fixed login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
   attr_writer :login
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
-  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
+  # validates :username, presence: true, uniqueness: { case_sensitive: false }
+  # validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, multiline: true
 
   has_many :user_interests, dependent: :destroy
   has_many :interests, through: :user_interests
@@ -20,7 +21,8 @@ class User < ApplicationRecord
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if (login = conditions.delete(:login))
-      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value",
+                                    { value: login.downcase }]).first
     elsif conditions.key?(:username) || conditions.key?(:email)
       where(conditions.to_h).first
     end
