ac28deea270034157420458fe05ad0999513913e
Samuel Anderson <samuelSD.999@gmail.com>
2024-03-12 19:28:31 +0000

    fixed login functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @user_tasks = current_user.user_tasks
+    # @user_tasks = current_user.user_tasks
   end
 end
