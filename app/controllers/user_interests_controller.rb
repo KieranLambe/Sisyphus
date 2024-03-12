7f8a02d7e372f185c74d7ac06a6b775b24ac7b7b
Samuel Anderson <samuelSD.999@gmail.com>
2024-03-12 04:10:18 +0000

    Added choose interests functionallity

--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -1,11 +1,31 @@
 class UserInterestsController < ApplicationController
   def index
-    @userInterests = UserInterests.new
+    @user_interests = current_user.user_interests.includes(:interest)
+  end
+
+  def new
+    @interests = Interest.all
+    @user_interest = UserInterest.new
+    # @user_interest.interest = @interests[0]
   end
 
   def create
+    @user_interest = UserInterest.new(user_interest_params)
+    @user_interest.user = current_user
+    @user_interest.interest = @interest
+    if @user_interest.save!
+      redirect_to root_path
+    else
+      raise
+    end
   end
 
   def updated
+  end
+
+  private
+
+  def user_interest_params
+    params.require(:user_interest).permit(:user_id, :interest_id)
   end
 end
