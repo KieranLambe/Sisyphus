e2cfa99897d827b2ec9ff9cc96755684f9a994d8
Samuel Anderson <samuelSD.999@gmail.com>
2024-03-14 20:47:37 +0000

    Tweaked user_tasks controller

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -64,7 +64,7 @@ class UserTasksController < ApplicationController
   end
 
   def user_task_params
-    params.require(:user_task).permit(:user_id, :task_id, :complete)
+    params.require(:user_task).permit(:user_id, :task_id)
   end
 
   def randomly_selected_tasks
