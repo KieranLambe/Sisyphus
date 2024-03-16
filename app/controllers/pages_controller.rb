35d302c2028f3a73d20bc4c7406312430c632497
Josue Heredia <josue-heredia@outlook.com>
2024-03-16 12:24:45 +0000

    search working for tasks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,14 +10,22 @@ class PagesController < ApplicationController
       @user_tasks = []
     end
 
-    @tasks = if params[:query].present?
-               Task.where("title ILIKE ?", "%#{params[:query]}%")
-             else
-               Task.includes(:interests).all.shuffle
-             end
+
+
+    # @tasks = if params[:query].present?
+    #            Task.where("title ILIKE ?", "%#{params[:query]}%")
+    #          else
+    #            Task.includes(:interests).all.shuffle
+    #          end
 
 
     @tasks = Task.page(params[:page]).per(21)
+
+    # respond_to do |format|
+    #   format.html
+    #   format.text { render partial: 'pages/lists', locals: { tasks: @tasks }, formats: [:html]}
+    # end
+
   end
 
 end
--- app/javascript/controllers/search_tasks_controller.js
+++ app/javascript/controllers/search_tasks_controller.js
@@ -5,20 +5,26 @@ export default class extends Controller {
   static targets = ["form", "query", "list"];
 
   connect() {
+    console.log(this.listTarget)
   }
 
-  // search(event) {
-  //   event.preventDefault();
-  //   console.log(this.queryTarget.value);
-  //   console.log(this.queryTarget.value);
-  //   console.log(this.formTarget.action);
-  //   console.log(this.listTarget);
-  //   const url = `${this.formTarget.action}?query=${this.queryTarget.value}`
-  //   fetch(url, {headers: {"Accept": "text/plain"}})
-  //     .then(response => response.text())
-  //     .then((data) => {
-  //       console.log(data);
-  //       this.listTarget.outerHTML = data
-  //     })
-  // }
+  search(event) {
+    // event.preventDefault();
+    // console.log(this.queryTarget.value);
+    console.log(this.queryTarget.value);
+    console.log(this.formTarget.action);
+    console.log(this.listTarget);
+    if (event.key === "Backspace" && !this.queryTarget.value.trim()) {
+      event.preventDefault();
+      return;
+    }
+    const url = `${this.formTarget.action}?query=${this.queryTarget.value}`
+
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        // console.log(data);
+        this.listTarget.outerHTML = data;
+      })
+  }
 }
