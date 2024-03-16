593922c75f983bdf1c3e07541ee7f7eedd387ce4
Samuel Anderson <samuelSD.999@gmail.com>
2024-03-16 12:17:32 +0000

    Created achievements and user_achievements controllers

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,7 +1,7 @@
 class Achievement < ApplicationRecord
-  validation :name, presence: true, uniqueness: true
-  validation :description, presence: true, uniqueness: true
-  validation :condition, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, uniqueness: true
+  validates :condition, presence: true, uniqueness: true
 
   has_many :user_achievements, dependent: :destroy
   has_many :users, through: :user_achievements
