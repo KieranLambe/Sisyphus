5473de372ff6c4f73f4139770a50214bfddccadb
Josue Heredia <josue-heredia@outlook.com>
2024-03-19 20:05:59 +0000

    all_can_see

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, except: [:index]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
