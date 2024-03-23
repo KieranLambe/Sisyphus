8e4591d0ccc3d14d89979dcc5a7bfa3a589b7a9d
Samuel Anderson <samuelSD.999@gmail.com>
2024-03-23 12:10:24 +0000

    Reformatted task search and achievements

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,13 +3,22 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all
+    # @tasks = Task.all
+    @tasks = Task.page(params[:page]).per(21)
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'pages/lists', locals: { tasks: @tasks }, formats: [:html]}
+    end
+
+    @interests = Interest.all
 
     @tasks = if params[:query].present?
-                Task.includes(:interests).joins(:interests).
-                where("tasks.title ILIKE ? OR tasks.description ILIKE ? OR interests.title ILIKE ? ", "%#{params[:query]}%", "%#{params[:query]}%","%#{params[:query]}%")
-              else
-                Task.includes(:interests).all.shuffle
+               Task.includes(:interests).joins(:interests)
+                   .where("tasks.title ILIKE ? OR tasks.description ILIKE ? OR interests.title ILIKE ? ",
+                          "%#{params[:query]}%", "%#{params[:query]}%", "%#{params[:query]}%")
+             else
+               Task.includes(:interests).all.shuffle
              end
 
     respond_to do |format|
