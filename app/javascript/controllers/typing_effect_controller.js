82050800199c3ce466fff200fc8210d18828f749
Josue Heredia <josue-heredia@outlook.com>
2024-03-28 21:29:58 +0000

    search bar changes

---
+++ app/javascript/controllers/typing_effect_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="typing-effect"
+export default class extends Controller {
+  static targets = ["placeholder"]
+  connect() {
+    this.placeholderElement = this.placeholderTarget;
+    this.placeholderText = this.placeholderElement.getAttribute("placeholder");
+    this.currentIndex = 0;
+    this.typingInterval = 100;
+    this.startTyping();
+  }
+
+  startTyping() {
+    this.typingIntervalId = setInterval(() => {
+      if (this.currentIndex <= this.placeholderText.length) {
+        const typedText = this.placeholderText.substring(0, this.currentIndex);
+        this.placeholderElement.setAttribute("placeholder", typedText);
+        this.currentIndex++;
+      } else {
+        clearInterval(this.typingIntervalId);
+        setTimeout(() => {
+          this.currentIndex = 0;
+          this.startTyping();
+        }, 3000);
+      }
+    }, this.typingInterval);
+  }
+
+}
