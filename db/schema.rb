c609bb9156f10278b9155915a4b5542ebc422c38
roselyn casio <roselyn.casio@gmail.com>
2024-03-30 10:10:57 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -71,9 +71,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_153903) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "user_achievements", force: :cascade do |t|
+  create_table "user_achievments", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "achievement_id", null: false
+    t.bigint "achievment_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "achieved", default: false
