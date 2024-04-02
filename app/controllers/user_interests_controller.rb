c5235a3a45f93e71efd26362d29de1543a862e87
Josue Heredia <josue-heredia@outlook.com>
2024-04-02 12:39:37 +0100

    add/delete interest working

--- app/controllers/user_interests_controller.rb
+++ app/controllers/user_interests_controller.rb
@@ -1,4 +1,6 @@
 class UserInterestsController < ApplicationController
+  before_action :set_user, only: [:destroy]
+  before_action :set_user_interest, only: [:destroy]
   # def index
   #   @user_interests = current_user.user_interests.includes(:interest)
   # end
@@ -10,11 +12,18 @@ class UserInterestsController < ApplicationController
 
   def create
     @user_interest = UserInterest.new(user_interest_params)
-    if @user_interest.save!
+    if @user_interest.save
       redirect_to new_user_user_interest_path
     else
+      @interests = Interest.all
       render 'new', status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @user_interest = UserInterest.find(params[:id])
+    @user_interest.destroy
+    redirect_to new_user_user_interest_path, notice: "User interest successfully deleted."
   end
 
   private
@@ -22,4 +31,13 @@ class UserInterestsController < ApplicationController
   def user_interest_params
     params.require(:user_interest).permit(:user_id, :interest_id)
   end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_user_interest
+    @user_interest = @user.user_interests.find(params[:id])
+  end
+
 end
