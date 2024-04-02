8bebb010a6655504b25210b7bc5cd2c118eb45ad
Serban Ciurea <ciureaserban777@icloud.com>
2024-04-02 20:00:11 +0100

    nice:

--- app/models/task.rb
+++ app/models/task.rb
@@ -9,6 +9,22 @@ class Task < ApplicationRecord
   has_many :user_tasks
   has_many :users, through: :user_tasks
 
+  # app/models/task.rb
+class Task < ApplicationRecord
+  # Assuming you have a column called image_title in your tasks table
+  def unsplash_image_url
+
+    response = HTTParty.get("https://api.unsplash.com/search/photos?query=#{CGI.escape(image_title)}&client_id=YOUR_UNSPLASH_API_KEY")
+    if response.code == 200 && !response.parsed_response["results"].empty?
+      response.parsed_response["results"][0]["urls"]["regular"]
+    else
+      # If no image found, return a default image URL or handle it as per your requirement
+      'https://example.com/default-image.jpg'
+    end
+  end
+end
+
+
   pg_search_scope :global_search,
     against: [:title, :description],
     associated_against: {
